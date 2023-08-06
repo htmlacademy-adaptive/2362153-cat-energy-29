b10c78e38cb795263bcec1e0418cbc96d65a1303
Isakova Mariya <bubawenka@gmail.com>
2023-08-07 00:31:20 +0300

    мобайл слайдер кот без кнопки готов

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,3 @@
-document.querySelector(".slider input").addEventListener("input", function(){
+document.querySelector(".slider__input").addEventListener("input", function(){
   document.querySelector(".slider__cat-slim").style.width = this.value + "%"
 })
