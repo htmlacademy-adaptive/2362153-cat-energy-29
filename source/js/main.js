f56693d7987f7dcd95b1aec9ba7dc65d1682ed3b
Isakova Mariya <bubawenka@gmail.com>
2023-08-08 09:41:53 +0300

    добавила меню бургер

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,13 @@
-document.querySelector(".slider__input").addEventListener("input", function(){
-  document.querySelector(".slider__cat-slim").style.width = this.value + "%"
+//Открытие меню
+let menuBtn = document.querySelector(".main-header__toggle");
+let menu = document.querySelector(".main-nav")
+menuBtn.addEventListener("click", function(){
+	menu.classList.toggle("main-nav--active");
+  menuBtn.classList.toggle("main-header__toggle--opened");
+  menuBtn.classList.toggle("main-header__toggle--close");
 })
+
+// Слайдер заготовка
+// document.querySelector(".slider__input").addEventListener("input", function(){
+//   document.querySelector(".slider__cat-slim").style.width = this.value + "%"
+// })
