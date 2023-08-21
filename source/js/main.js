a1fa6c10bda78ccae5017f472599e41b299aa74b
Isakova Mariya <bubawenka@gmail.com>
2023-08-21 14:05:19 +0300

    переделала спрайты под сборку

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,10 @@
 //Открытие меню
 
 let menuBtn = document.querySelector(".main-header__toggle");
-let menu = document.querySelector(".main-header__nav");
+let menu = document.querySelector(".main-nav");
 
 menu.classList.remove("main-nav--nojs");
-menu.classList.add("main-nav");
-document.querySelector(".main-nav__list").style.position = "absolute";
-menuBtn.classList.add("main-header__toggle--close");
-menuBtn.style.display = "block";
-
-if(window.innerWidth < 767) {
-  menuBtn.style.display = "block";
-  document.querySelector(".main-header__wrapper").style.borderBottom = "1px solid var(--color-grey450)";
-} else {
-  menuBtn.style.display = "none";
-};
+menuBtn.classList.remove("main-nav--nojs");
 
 menuBtn.addEventListener("click", function() {
   menu.classList.toggle("main-nav--active");
